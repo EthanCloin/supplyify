bb3581fb49e41476100b3a031cd81f8d82fc64f0
Ethan Cloin <psnethan@gmail.com>
2025-05-24 02:43:33 -0400

    Order Management:
    - Add edit order view
    - Add order details page
    - Add db support for edits to order

--- app/main.py
+++ app/main.py
@@ -30,6 +30,66 @@ def get_orders():
     return render_template("orders-manage.html", orders=all_orders)
 
 
+@bp.route("/orders/<int:order_id>", methods=["GET", "PUT"])
+def get_order_details(order_id):
+    cxn = db.get_db()
+    order_details = orders.get_order(cxn, order_id)
+    products = orders.get_products(cxn, order_id)
+
+    if request.method == "PUT":
+        updated_order = {
+            "name": request.form.get("order-name"),
+            "status": request.form.get("order-status"),
+        }
+        updated_products = extract_product_updates()
+
+        affected_order = orders.update_order(cxn, order_id, updated_order)
+        affected_products = orders.update_products(cxn, order_id, updated_products)
+        order_details = orders.get_order(cxn, order_id)
+        products = orders.get_products(cxn, order_id)
+
+    if request.headers.get("HX-Request"):
+        return render_template(
+            "order-detail.html", order=order_details, products=products
+        )
+    return render_template(
+        "order-detail-page.html", order=order_details, products=products
+    )
+
+
+def extract_product_updates() -> dict[int, dict[str, int]]:
+    """used to process request updating product assignments on an order"""
+    updated_products = {}
+    for k, v in request.form.items():
+        if k.startswith("product-requested"):
+            op_id = int(k.split("-")[-1])
+            updated_products[op_id] = updated_products.get(op_id, {}) | {
+                "requested": int(v)
+            }
+        if k.startswith("product-allocated"):
+            op_id = int(k.split("-")[-1])
+            updated_products[op_id] = updated_products.get(op_id, {}) | {
+                "allocated": int(v)
+            }
+    return updated_products
+
+
+@bp.route("/orders/<int:order_id>/edit")
+def order_edit_form(order_id, methods=["GET", "PUT"]):
+
+    cxn = db.get_db()
+    order_details = orders.get_order(cxn, order_id)
+    products = orders.get_products(cxn, order_id)
+
+    if request.headers.get("HX-Request"):
+        return render_template(
+            "order-edit.html", order=order_details, products=products
+        )
+    return render_template(
+        "order-edit-page.html", order=order_details, products=products
+    )
+
+
 def get_dashboard_data():
     cxn = db.get_db()
     all_orders = orders.get_all_orders(cxn)
--- app/orders.py
+++ app/orders.py
@@ -29,6 +29,19 @@ GROUP BY Status"""
     return counts
 
 
+def get_order(db: Connection, order_id: int):
+
+    query = """SELECT
+    OrderID 
+    ,Name
+    ,Status
+FROM Orders
+WHERE OrderID = ?;
+"""
+    order = db.execute(query, (order_id,)).fetchone()
+    return order
+
+
 def get_products(db: Connection, order_id: int):
     query = """
 SELECT 
@@ -37,6 +50,7 @@ SELECT
     ,op.AllocatedToOrder as Allocated
     ,p.UnitsStocked as Available
     ,(op.RequestedForOrder - op.AllocatedToOrder) As Unfulfilled
+    ,op.OrderProductsID
 FROM Products p
 JOIN OrderProducts op ON op.ProductID = p.ProductID
 WHERE op.OrderID = ?
@@ -44,3 +58,40 @@ ORDER BY p.Name ASC;
 """
     rows = db.execute(query, (order_id,)).fetchall()
     return rows
+
+
+def update_order(db: Connection, order_id: int, order: dict):
+    query = """
+UPDATE Orders
+SET 
+    Name = ?
+    ,Status = ?
+WHERE OrderID = ?
+RETURNING OrderID;
+"""
+    #
+    affected_order = db.execute(
+        query, (order.get("name"), order.get("status"), order_id)
+    ).fetchone()
+    db.commit()
+    return affected_order
+
+
+def update_products(
+    db: Connection, order_id: int, order_products: dict[int, dict[str, int]]
+):
+    query = """
+UPDATE OrderProducts
+SET
+    RequestedForOrder = ?
+    ,AllocatedToOrder = ?
+WHERE
+    OrderProductsID = ?
+RETURNING OrderProductsID
+    """
+    affected_products = []
+    for op_id, vals in order_products.items():
+        params = (vals.get("requested"), vals.get("allocated"), op_id)
+        affected_products.append(db.execute(query, params).fetchone())
+    db.commit()
+    return affected_products
