9e33b4f99098e404649dd5d6f7e4d66c96f963d0
Ethan Cloin <62920769+EthanCloin@users.noreply.github.com>
2025-05-25 00:09:25 -0400

    Client Interactivity + Order Management (#3)
    Client Interactivity
    * Add HTMX
    * Clicking orders updates product table
    * Clicking 'create' dropsdown menu in nav
    * Added orders summary page
    - View functions render full page on regular request, but only the fragment when triggered by HTMX
    
    Order Management
    - Add edit order view
    - Add order details page
    - Add db support for edits to order
    - Manage page includes links to Detail and Edit views for each order
    - Detail page links back to Manage and is 'click to edit'

--- app/main.py
+++ app/main.py
@@ -7,6 +7,7 @@ bp = Blueprint("main", __name__)
 
 
 @bp.route("/")
+@bp.route("/dashboard")
 def home():
     dashboard = get_dashboard_data()
     return render_template("dashboard.html", **dashboard)
@@ -20,6 +21,75 @@ def get_products():
         products = get_products_on_order(order_id)
         return render_template("order-products-table.html", products=products)
     return "<tbody></tbody>"
+  
+
+@bp.route("/orders")
+def get_orders():
+    cxn = db.get_db()
+    all_orders = orders.get_all_orders(cxn)
+    if request.headers.get("HX-Request"):
+        return render_template("orders-manage.html", orders=all_orders)
+    return render_template("orders-manage-page.html", orders=all_orders)
+
+
+@bp.route("/orders/<int:order_id>", methods=["GET", "PUT"])
+def get_order_details(order_id):
+    cxn = db.get_db()
+    order_details = orders.get_order(cxn, order_id)
+    products = orders.get_products(cxn, order_id)
+
+    if request.method == "PUT":
+        updated_order = {
+            "name": request.form.get("order-name"),
+            "status": request.form.get("order-status"),
+        }
+        updated_products = extract_product_updates()
+
+        affected_order = orders.update_order(cxn, order_id, updated_order)
+        affected_products = orders.update_products(cxn, order_id, updated_products)
+        order_details = orders.get_order(cxn, order_id)
+        products = orders.get_products(cxn, order_id)
+
+    if request.headers.get("HX-Request"):
+        return render_template(
+            "order-detail.html", order=order_details, products=products
+        )
+    return render_template(
+        "order-detail-page.html", order=order_details, products=products
+    )
+
+
+def extract_product_updates() -> dict[int, dict[str, int]]:
+    """used to process request updating product assignments on an order"""
+    updated_products = {}
+    for k, v in request.form.items():
+        if k.startswith("product-requested"):
+            op_id = int(k.split("-")[-1])
+            updated_products[op_id] = updated_products.get(op_id, {}) | {
+                "requested": int(v)
+            }
+        if k.startswith("product-allocated"):
+            op_id = int(k.split("-")[-1])
+            updated_products[op_id] = updated_products.get(op_id, {}) | {
+                "allocated": int(v)
+            }
+    return updated_products
+
+
+@bp.route("/orders/<int:order_id>/edit")
+def order_edit_form(order_id, methods=["GET", "PUT"]):
+
+    cxn = db.get_db()
+    order_details = orders.get_order(cxn, order_id)
+    products = orders.get_products(cxn, order_id)
+
+    if request.headers.get("HX-Request"):
+        return render_template(
+            "order-edit.html", order=order_details, products=products
+        )
+    return render_template(
+        "order-edit-page.html", order=order_details, products=products
+    )
 
 
 def get_dashboard_data():
--- app/orders.py
+++ app/orders.py
@@ -21,12 +21,24 @@ FROM Orders
 WHERE Status IN ('Open', 'Procurement', 'Production')
 GROUP BY Status"""
     rows = db.execute(query).fetchall()
-    counts = {
-        "open": rows[0]["count"],
-        "procurement": rows[1]["count"],
-        "production": rows[2]["count"],
-    }
+    counts = {"open": 0, "procurement": 0, "production": 0}
+    for r in rows:
+        counts[r["status"].lower()] = r["count"]
+
     return counts
+
+
+def get_order(db: Connection, order_id: int):
+
+    query = """SELECT
+    OrderID 
+    ,Name
+    ,Status
+FROM Orders
+WHERE OrderID = ?;
+"""
+    order = db.execute(query, (order_id,)).fetchone()
+    return order
 
 
 def get_products(db: Connection, order_id: int):
@@ -37,6 +49,7 @@ SELECT
     ,op.AllocatedToOrder as Allocated
     ,p.UnitsStocked as Available
     ,(op.RequestedForOrder - op.AllocatedToOrder) As Unfulfilled
+    ,op.OrderProductsID
 FROM Products p
 JOIN OrderProducts op ON op.ProductID = p.ProductID
 WHERE op.OrderID = ?
@@ -44,3 +57,40 @@ ORDER BY p.Name ASC;
 """
     rows = db.execute(query, (order_id,)).fetchall()
     return rows
+
+
+def update_order(db: Connection, order_id: int, order: dict):
+    query = """
+UPDATE Orders
+SET 
+    Name = ?
+    ,Status = ?
+WHERE OrderID = ?
+RETURNING OrderID;
+"""
+    #
+    affected_order = db.execute(
+        query, (order.get("name"), order.get("status"), order_id)
+    ).fetchone()
+    db.commit()
+    return affected_order
+
+
+def update_products(
+    db: Connection, order_id: int, order_products: dict[int, dict[str, int]]
+):
+    query = """
+UPDATE OrderProducts
+SET
+    RequestedForOrder = ?
+    ,AllocatedToOrder = ?
+WHERE
+    OrderProductsID = ?
+RETURNING OrderProductsID
+    """
+    affected_products = []
+    for op_id, vals in order_products.items():
+        params = (vals.get("requested"), vals.get("allocated"), op_id)
+        affected_products.append(db.execute(query, params).fetchone())
+    db.commit()
+    return affected_products
